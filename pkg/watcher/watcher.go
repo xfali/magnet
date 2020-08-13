ad807dc20f3d2d7add52e7884df7b36a442703da
xfali <damao1222@live.com>
2020-08-13 22:14:12 +0800

    增加时间内过滤去重的文件监听器

--- pkg/watcher/watcher.go
+++ pkg/watcher/watcher.go
@@ -12,6 +12,7 @@ import (
 	"github.com/xfali/stream"
 	"log"
 	"sync"
+	"time"
 )
 
 type PackageListener interface {
@@ -100,6 +101,76 @@ func (w *PackageWatcher) Watch(p installer.Package) {
 			case err := <-watch.Errors:
 				log.Println("error : ", err)
 				return
+			case <-w.stop:
+				return
+			}
+		}
+	}()
+}
+
+type PackageBatchWatcher struct {
+	PackageWatcher
+
+	interval time.Duration
+}
+
+func NewPackageBatchWatcher(interval time.Duration) *PackageBatchWatcher {
+	return &PackageBatchWatcher{
+		PackageWatcher: *NewWatcher(),
+		interval:interval,
+	}
+}
+
+func (w *PackageBatchWatcher) Watch(p installer.Package) {
+	//创建一个监控对象
+	watch, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = watch.Add(p.GetInstallPath())
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		ticker := time.NewTicker(w.interval)
+		var events []fsnotify.Event
+		defer watch.Close()
+		defer ticker.Stop()
+
+		for {
+			select {
+			case ev := <-watch.Events:
+				events = append(events, ev)
+			case err := <-watch.Errors:
+				log.Println("error : ", err)
+				return
+			case <-ticker.C:
+				events = stream.Slice(events).Distinct(func(ev1, ev2 fsnotify.Event) bool {
+					return ev1.Name == ev2.Name && ev1.Op == ev2.Op
+				}).Collect(nil).([]fsnotify.Event)
+				for _, ev := range events {
+					if ev.Op&fsnotify.Create == fsnotify.Create {
+						//stream.Slice(w.getListeners()).Foreach(func(l PackageListener) {
+						//	l.OnCreate(p, ev.Name)
+						//})
+					}
+					if ev.Op&fsnotify.Write == fsnotify.Write {
+						stream.Slice(w.getListeners()).Foreach(func(l PackageListener) {
+							l.OnUpdate(p, ev.Name)
+						})
+					}
+					if ev.Op&fsnotify.Remove == fsnotify.Remove {
+						stream.Slice(w.getListeners()).Foreach(func(l PackageListener) {
+							l.OnRemove(p, ev.Name)
+						})
+						if ev.Name == p.GetInstallPath() {
+							log.Println("package removed, exit watch")
+							return
+						}
+					}
+				}
+				events = []fsnotify.Event{}
 			case <-w.stop:
 				return
 			}
--- test/watch_test.go
+++ test/watch_test.go
@@ -9,10 +9,22 @@ import (
 	"github.com/xfali/magnet/pkg/installer"
 	"github.com/xfali/magnet/pkg/watcher"
 	"testing"
+	"time"
 )
 
 func TestWatcher(t *testing.T) {
-	w := watcher.PackageWatcher{}
+	w := watcher.NewWatcher()
+	w.AddListener(&watcher.DummyListener{})
+	pkg := installer.ZipPackage{
+		InstallPath: "target/test",
+	}
+	w.Watch(&pkg)
+	//<-time.NewTimer(30 * time.Second).C
+	select {}
+}
+
+func TestPackageBatchWatcher(t *testing.T) {
+	w := watcher.NewPackageBatchWatcher(1*time.Second)
 	w.AddListener(&watcher.DummyListener{})
 	pkg := installer.ZipPackage{
 		InstallPath: "target/test",
